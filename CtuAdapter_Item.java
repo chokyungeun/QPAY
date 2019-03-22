95266df0633db41a288c51eac30d4e4481d51086
chokyungeun <32236350+chokyungeun@users.noreply.github.com>
2019-03-22 17:18:39 +0900

    Create CtuAdapter_Item.java

---
+++ CtuAdapter_Item.java
@@ -0,0 +1,20 @@
+package com.hfad.qpay.lout;
+
+public class CtuAdapter_Item {
+    private String Item ;
+    private String Price ;
+
+    public void setItem(String item) {
+        Item = item ;
+    }
+    public void setPrice(String price) {
+        Price = price ;
+    }
+
+    public String getItem() {
+        return this.Item ;
+    }
+    public String getPrice() {
+        return this.Price ;
+    }
+}
